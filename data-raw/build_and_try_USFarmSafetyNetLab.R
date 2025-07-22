2382b9e79ecdcc9b2ff9af02a609787af0fd7e0e
Francis Tsiboe <69377090+ftsiboe@users.noreply.github.com>
2025-07-22 08:49:52 -0500

    Add USFarmSafetyNetLab build script and update .gitignore
    Added data-raw/build_and_try_USFarmSafetyNetLab.R for building and testing the USFarmSafetyNetLab project. Updated .gitignore to include this new script and ensure it is not ignored.

---
+++ data-raw/build_and_try_USFarmSafetyNetLab.R
@@ -0,0 +1,22 @@
+# remotes::install_github("dylan-turner25/rmaADM", force = TRUE,upgrade="never")
+# remotes::install_github("dylan-turner25/rfcip", force = TRUE,upgrade="never")
+# remotes::install_github("dylan-turner25/rfsa", force = TRUE,upgrade="never")
+
+# Run in your project:
+rm(list=ls(all=TRUE));gc()
+# library(rmaADM)
+# devtools::load_all("C:/GitHub/rfcipCalcPass")
+# devtools::load_all("C:/GitHub/rfcipCalibrate")
+unlink(c("NAMESPACE",list.files("./man", full.names = TRUE)))
+#source("data-raw/internal_datasets.R")
+devtools::document()
+for(i in list.files("R",full.names = T)){
+  print(paste0("********************",i,"********************"))
+  tools::showNonASCIIfile(i)
+}
+devtools::check_man()
+devtools::build_manual(path = getwd())
+devtools::test()
+devtools::check()
+
+
