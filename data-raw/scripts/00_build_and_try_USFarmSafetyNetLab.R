334c3aaa8191eaebae8756cb0e5db488cc542a2a
Francis Tsiboe <69377090+ftsiboe@users.noreply.github.com>
2025-09-09 23:49:09 -0500

    Add SOB-TPU cleaning and supplemental plan share functions
    Introduces new R functions for cleaning and aggregating RMA SOB-TPU data, calculating SCO/ECO shares, and setting up project environments. Updates NAMESPACE and DESCRIPTION to export and import relevant functions and dependencies. Adds documentation and example scripts for data release and environment setup.

--- data-raw/scripts/00_build_and_try_USFarmSafetyNetLab.R
+++ data-raw/scripts/00_build_and_try_USFarmSafetyNetLab.R
@@ -2,7 +2,7 @@
 # remotes::install_github("dylan-turner25/rfcip", force = TRUE,upgrade="never")
 # remotes::install_github("dylan-turner25/rfsa", force = TRUE,upgrade="never")
 # usethis::use_github_action()
-rm(list=ls(all=TRUE));gc();library(rfcip);library(rmaADM);library(data.table);library(stringr)
+rm(list=ls(all=TRUE));gc();library(rfcip);library(data.table);library(stringr)
 unlink(c("NAMESPACE","./R/helper_data.R",
          list.files("./data", full.names = TRUE),
          list.files("./man", full.names = TRUE)))
@@ -15,6 +15,4 @@ for(i in list.files("R",full.names = T)){
 }
 devtools::check_man()
 devtools::build_manual(path = getwd())
-devtools::test()
-
 devtools::check()
