fc130e9d03c94431a01ecc45c276a9c9dee084da
Francis Tsiboe <69377090+ftsiboe@users.noreply.github.com>
2025-10-10 16:12:00 -0500

    updates

--- data-raw/scripts/00_build_and_try_USFarmSafetyNetLab.R
+++ data-raw/scripts/00_build_and_try_USFarmSafetyNetLab.R
@@ -1,4 +1,3 @@
-# remotes::install_github("dylan-turner25/rmaADM", force = TRUE,upgrade="never")
 # remotes::install_github("dylan-turner25/rfcip", force = TRUE,upgrade="never")
 # remotes::install_github("dylan-turner25/rfsa", force = TRUE,upgrade="never")
 # usethis::use_github_action()
@@ -9,10 +8,12 @@ unlink(c("NAMESPACE","./R/helper_data.R",
 #source("data-raw/scripts/build_internal_datasets.R")
 rm(list=ls(all=TRUE))
 devtools::document()
-for(i in list.files("R",full.names = T)){
+for(i in list.files("R",full.names = T,recursive = T)){
   print(paste0("********************",i,"********************"))
   tools::showNonASCIIfile(i)
 }
 devtools::check_man()
 devtools::build_manual(path = getwd())
 devtools::check()
+
+
